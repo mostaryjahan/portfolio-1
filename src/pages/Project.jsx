860f9def1238864703267dfd9f7a4354f9d92795
Mostary Jahan <mostaryjahan01@gmail.com>
2024-07-04 17:15:05 +0600

    aaadd

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,9 +1,6 @@
 import study from "../assets/study-crop.png";
 import art from "../assets/art-1.png";
-import medi from "../assets/medi-crop.png"
-
-
-
+import medi from "../assets/medi-crop.png";
 
 const Project = () => {
   return (
@@ -17,54 +14,143 @@ const Project = () => {
 
       <div className="grid grid-cols- dark:text-black md:grid-cols-3 gap-6 mt-10  p-2 ">
         {/* card 1 */}
-        <div className="card rounded glass bg-slate-200 ">
+        <div className="card flex flex-col rounded glass bg-slate-200 ">
           <figure className="  ">
-            <img src={medi} alt="car!" className="rounded-t h-[300px] w-full " />
+            <img
+              src={medi}
+              alt="car!"
+              className="rounded-t h-[300px] w-full "
+            />
           </figure>
-          <div className="p-4 ">
-            <h2 className="card-title dark:text-black font-mon">Medi Corner</h2>
-            <p className="dark:text-black font-mon">Medi Corner is a website for a multi-vendor e-commerce platform specializing in the sale of medicines and healthcare products.</p>
-            <p className="font-mon mt-2"><span className="font-semibold">Features:</span> User Authentication, Payment Integration, Data Management</p>
-            <p className="font-mon mt-2 mb-2"><span className="font-semibold">Technology: </span>Tanstack query ,Axios, React,React-router-dom, MongoDB, Jwt, Firebase, Node.js</p>
-            <div className=" card-actions md:flex justify-center">
-             <a href="https://github.com/mostaryjahan/medi-client-a12" className="px-4 py-2 rounded-3xl dark:text-white bg-black text-white text-xs">Client Code</a>
-             <a href="https://github.com/mostaryjahan/medi-server-a12" className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs">Server Code</a>
-             <a href="https://medi-corner-22.web.app" className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs">Live Site</a>
+          <div className="p-4 flex flex-col flex-1">
+           <div className="flex-1">
+           <h2 className="card-title dark:text-black font-mon">Medi Corner</h2>
+            <p className="dark:text-black font-mon">
+              Medi Corner is a website for a multi-vendor e-commerce platform
+              specializing in the sale of medicines and healthcare products.
+            </p>
+            <p className="font-mon mt-2">
+              <span className="font-semibold">Features:</span> User
+              Authentication, Payment Integration, Data Management
+            </p>
+            <p className="font-mon mt-2 mb-2">
+              <span className="font-semibold">Technology: </span>Tanstack query
+              ,Axios, React,React-router-dom, MongoDB, Jwt, Firebase, Node.js
+            </p>
+           </div>
+            <div className=" card-actions justify-self-end md:flex justify-center">
+              <a
+                href="https://github.com/mostaryjahan/medi-client-a12"
+                className="px-4 py-2 rounded-3xl dark:text-white bg-black text-white text-xs"
+              >
+                Client Code
+              </a>
+              <a
+                href="https://github.com/mostaryjahan/medi-server-a12"
+                className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs"
+              >
+                Server Code
+              </a>
+              <a
+                href="https://medi-corner-22.web.app"
+                className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs"
+              >
+                Live Site
+              </a>
             </div>
           </div>
         </div>
 
         {/* card 2 */}
-        <div className="card glass rounded  bg-slate-200 ">
+        <div className="card glass flex flex-col rounded  bg-slate-200 ">
           <figure className=" ">
-            <img src={study} alt="car!" className="rounded-t h-[300px] w-full " />
+            <img
+              src={study}
+              alt="car!"
+              className="rounded-t h-[300px] w-full "
+            />
           </figure>
-          <div className="p-4 ">
-          <h2 className="card-title dark:text-black font-mon">StudyHub</h2>
-            <p className="dark:text-black font-mon">StudyHub is an online platform designed to help students enhance their learning experience and academic performance. </p>
-            <p className="font-mon mt-2"><span className="font-semibold">Features:</span> User Authentication, Assignment Creation, Assignment Management</p>
-            <p className="font-mon mt-2 mb-2"><span className="font-semibold">Technology: </span>Axios, React,React-router-dom, MongoDB, Jwt, Firebase, Node.js</p>
-            <div className="card-actions md:flex justify-center">
-             <a href="https://github.com/mostaryjahan/group-study-client-a11" className="px-4 py-2 rounded-3xl dark:text-white bg-black text-white text-xs">Client Code</a>
-             <a href="https://github.com/mostaryjahan/group-study-server-a11" className="px-4 py-2 bg-black  text-white rounded-3xl dark:text-white text-xs">Server Code</a>
-             <a href="https://group-study-a15f6.web.app/" className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs">Live Site</a>
+          <div className="p-4 flex flex-col flex-1">
+            <div className="flex-1">
+              <h2 className="card-title dark:text-black font-mon">StudyHub</h2>
+              <p className="dark:text-black font-mon">
+                StudyHub is an online platform designed to help students enhance
+                their learning experience and academic performance.{" "}
+              </p>
+              <p className="font-mon mt-2">
+                <span className="font-semibold">Features:</span> User
+                Authentication, Assignment Creation, Assignment Management
+              </p>
+              <p className="font-mon mt-2 mb-2">
+                <span className="font-semibold">Technology: </span>Axios,
+                React,React-router-dom, MongoDB, Jwt, Firebase, Node.js
+              </p>
+            </div>
+            <div className="card-actions  justify-self-end md:flex justify-center">
+              <a
+                href="https://github.com/mostaryjahan/group-study-client-a11"
+                className="px-4 py-2 rounded-3xl dark:text-white bg-black text-white text-xs"
+              >
+                Client Code
+              </a>
+              <a
+                href="https://github.com/mostaryjahan/group-study-server-a11"
+                className="px-4 py-2 bg-black  text-white rounded-3xl dark:text-white text-xs"
+              >
+                Server Code
+              </a>
+              <a
+                href="https://group-study-a15f6.web.app/"
+                className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs"
+              >
+                Live Site
+              </a>
             </div>
           </div>
         </div>
         {/* card 3 */}
-        <div className="card glass rounded border-2 border-gray-600 bg-slate-200 ">
+        <div className="card glass flex flex-col rounded border-2 border-gray-600 bg-slate-200 ">
           <figure className="">
             <img src={art} alt="car!" className="rounded-t h-[300px] w-full " />
           </figure>
-          <div className="p-4">
-          <h2 className=" dark:text-black text-xl font-semibold font-mon">Artz</h2>
-            <p className="dark:text-black font-mon">Artz is a web site focusing on Painting and Drawing. Artz allows users to browse various painting and drawing items.</p>
-            <p className="font-mon mt-2"><span className="font-semibold">Features:</span> User Authentication, Category Browsing, Responsive Design</p>
-            <p className="mt-2 mb-2 font-mon"><span className="font-semibold ">Technology:</span> React-router-dom, MongoDB,  Firebase for environment variables, Node.js</p>
-            <div className="card-actions  md:flex justify-center">
-             <a href="https://github.com/mostaryjahan/art-client-a10" className="px-4 py-2 rounded-3xl dark:text-white bg-black text-white text-xs">Client Code</a>
-             <a href="https://github.com/mostaryjahan/art-server-a10" className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs">Server Code</a>
-             <a href="https://art-store-64ab6.web.app" className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs">Live Site</a>
+          <div className="p-4 flex-1 flex flex-col ">
+          <div className="flex-1">
+          <h2 className=" dark:text-black text-xl font-semibold font-mon">
+              Artz
+            </h2>
+            <p className="dark:text-black font-mon">
+              Artz is a web site focusing on Painting and Drawing. Artz allows
+              users to browse various painting and drawing items.
+            </p>
+            <p className="font-mon mt-2">
+              <span className="font-semibold">Features:</span> User
+              Authentication, Category Browsing, Responsive Design
+            </p>
+            <p className="mt-2 mb-2 font-mon">
+              <span className="font-semibold ">Technology:</span>{" "}
+              React-router-dom, MongoDB, Firebase for environment variables,
+              Node.js
+            </p>
+          </div>
+            <div className="card-actions md:flex justify-center">
+              <a
+                href="https://github.com/mostaryjahan/art-client-a10"
+                className="px-4 py-2 rounded-3xl dark:text-white bg-black text-white text-xs"
+              >
+                Client Code
+              </a>
+              <a
+                href="https://github.com/mostaryjahan/art-server-a10"
+                className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs"
+              >
+                Server Code
+              </a>
+              <a
+                href="https://art-store-64ab6.web.app"
+                className="px-4 py-2 bg-black text-white rounded-3xl dark:text-white text-xs"
+              >
+                Live Site
+              </a>
             </div>
           </div>
         </div>
